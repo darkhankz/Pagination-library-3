ded60b9d5749ecf6d737c9614b08bdeb8d6d7e65
darkhankz <upstudynow@gmail.com>
2023-01-24 16:28:54 +0600

    the end Mainfragment

---
+++ app/src/main/java/com/movies/pagination_library_3/view/MainFragment.kt
@@ -0,0 +1,83 @@
+package com.movies.pagination_library_3.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
+import com.movies.pagination_library_3.databinding.FragmentMainBinding
+import com.movies.pagination_library_3.model.repository.MainRepository
+import com.movies.pagination_library_3.model.repository.MyViewModelFactory
+import com.movies.pagination_library_3.model.retrofit.ApiInterface
+import com.movies.pagination_library_3.view.adapter.MoviePagerAdapter
+import com.movies.pagination_library_3.viewModel.MainViewModel
+import kotlinx.coroutines.launch
+
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: MainViewModel
+    private val adapter = MoviePagerAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val retrofitService = ApiInterface.create()
+        val mainRepository = MainRepository(retrofitService)
+        binding.recyclerViewFragment.adapter = adapter
+
+        viewModel = ViewModelProvider(
+            this,
+            MyViewModelFactory(mainRepository)
+        )[MainViewModel::class.java]
+
+        viewModel.errorMessage.observe(viewLifecycleOwner) {
+            Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
+        }
+
+        adapter.addLoadStateListener { loadState ->
+            // show empty list
+            if (loadState.refresh is LoadState.Loading ||
+                loadState.append is LoadState.Loading)
+                binding.progressDialog.isVisible = true
+            else {
+                binding.progressDialog.isVisible = false
+                // If we have an error, show a toast
+                val errorState = when {
+                    loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+                    loadState.prepend is LoadState.Error ->  loadState.prepend as LoadState.Error
+                    loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+                    else -> null
+                }
+                errorState?.let {
+                    Toast.makeText(activity, it.error.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.getMovieList().observe(viewLifecycleOwner) {
+                it?.let {
+                    adapter.submitData(lifecycle, it)
+                }
+            }
+        }
+
+    }
+
+}
